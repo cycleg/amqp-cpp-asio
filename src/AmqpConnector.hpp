e2321cfb229cfb91c1aba0bf1a66f023aa2932e4
Alexei Golovin <golovin.alexei@gmail.com>
2018-05-27 13:18:48 +0300

    Documentaion on the march.

--- src/AmqpConnector.hpp
+++ src/AmqpConnector.hpp
@@ -18,7 +18,7 @@ class Connector
 {
   public:
     ///
-    /// Коды завершения.
+    /// Коды завершения коннектора.
     ///
     enum ExitCode
     {
@@ -35,34 +35,131 @@ class Connector
     /// Тип списка приемопередатчиков.
     ///
     typedef std::list<TransceiverPtr> TransceiverList;
+    ///
+    /// Итератор списка приемопередатчиков.
+    /// 
     typedef typename TransceiverList::iterator iterator;
 
+    ///
+    /// Указатель на функцию обратного вызова при успешном запуске коннектора.
+    ///
     typedef std::function<void()> StartedCallback;
+    ///
+    /// Указатель на функцию обратного вызова при отключении коннектора.
+    ///
+    /// @param [in] ExitCode Код завершения коннектора.
+    ///
     typedef std::function<void(ExitCode)> ExitCallback;
 
+    ///
+    /// Конструктор.
+    ///
+    /// @param [in] service Ссылка на экземпляр службы ввода/вывода.
+    /// @param [in] brokerUrl URL брокера, с которым работает коннектор.
+    ///
     Connector(boost::asio::io_service& service, std::string brokerUrl);
+    ///
+    /// Деструктор.
+    ///
     ~Connector();
 
+    ///
+    /// Копирующий конструктор запрещен.
+    ///
     Connector(const Connector&) = delete;
 
+    ///
+    /// Готовность коннектора к работе с брокером AMQP.
+    ///
+    /// @return Готов или нет.
+    ///
     inline bool ready() const { return m_connectionHandlerReady; }
 
+    ///
+    /// Назначить обратный вызов для завершения коннектора.
+    ///
+    /// @param [in] callback Указатель на функцию обратного вызова.
+    ///
     inline void onExit(ExitCallback callback) { m_exitCb = callback; }
+    ///
+    /// Получить указатель на текущую функцию обратного вызова.
+    ///
+    /// @return Указатель на функцию обратного вызова.
+    ///
     inline ExitCallback getOnExit() const { return m_exitCb; }
+    ///
+    /// Извлечь ссылку на службу ввода/вывода, используемую коннектором.
+    ///
+    /// @return Ссылка на экземпляр службы ввода/вывода.
+    ///
     inline boost::asio::io_service& io_service() { return m_service; }
+    ///
+    /// Получить URL на брокер, с которым работает коннектором.
+    ///
+    /// @return URL брокера AMQP.
+    ///
+    /// В URL может отсутствовать номер порта, если он совпадает с портом ФЬЙЗ
+    /// по умолчанию.
+    ///
     inline std::string url() const { return std::string(m_address); }
 
+    ///
+    /// Начало списка приемопередатчиков коннектора.
+    ///
+    /// @return Итератор начала списка.
+    /// 
     inline iterator begin() { return m_transceivers.begin(); }
+    ///
+    /// Конец списка приемопередатчиков коннектора.
+    ///
+    /// @return Итератор конца списка.
+    /// 
     inline iterator end() { return m_transceivers.end(); }
 
+    ///
+    /// Создать приемопередатчик с указанными параметрами.
+    ///
+    /// @param [in] exchange Имя точки обмена AMQP.
+    /// @param [in] queue_ Имя очереди сообщений.
+    /// @param [in] route_in Маршрут входящих сообщений.
+    /// @param [in] listener Флаг, что экземпляр будет работать на прием.
+    /// @return Итератор вновь созданного приемопередатчика.
+    ///
+    /// Если новый экземпляр работает только на передачу, имя очереди и
+    /// маршрут игнорируются.
+    ///
     iterator transceiver(const std::string& exchange,
                          const std::string& queue_,
                          const std::string& route_in,
                          bool listener);
+    ///
+    /// Включить указанный приемопередатчик.
+    ///
+    /// @param [in] i Итератор приемопередатчика.
+    ///
     void open(iterator i);
+    ///
+    /// Выключить указанный приемопередатчик.
+    ///
+    /// @param [in] i Итератор приемопередатчика.
+    ///
     void close(iterator i);
+    ///
+    /// Удалить указанный приемопередатчик.
+    ///
+    /// @param [in] i Итератор приемопередатчика.
+    ///
     void remove(iterator i);
 
+    ///
+    /// Отправить сообщение через указанный приемопередатчик.
+    ///
+    /// @param [in] i Итератор приемопередатчика.
+    /// @param [in] message Исходящее сообщение.
+    /// @param [in] route Маршрут отправки.
+    /// @param [in] mandatory Флаг "mandatory" (необязательный, по умолчанию
+    ///                       установлен).
+    ///
     template<class Message>
     bool send(iterator i, const Message& message,
               const std::string& route, bool mandatory = true)
@@ -72,11 +169,26 @@ class Connector
       return (*i)->send(message, route, mandatory);
     }
 
+    ///
+    /// Инициировать работу с брокером асинхронно.
+    ///
+    /// @param [in] callback Функция обратного вызова для успешного
+    ///                      завершения процедуры (необязательный, по
+    ///                      умолчанию отсутствует).
+    ///
+    /// В случае неудачного подключения запускается функция обратного вызова
+    /// на завершение работы конектора с кодом eBrokerConnectError.
+    ///
+    /// Если ни один обратный вызов не задан, готовность коннектора к работе с
+    /// брокером можно определить по значению ready().
+    ///
     void async_start(StartedCallback callback = nullptr);
     ///
     /// Инициировать работу с брокером.
     ///
-    /// Синхронная версия async_start().
+    /// Синхронная версия async_start(). Заверашется запуском одной из функций
+    /// обратного вызова: на звершение работы коннектора или на успешное
+    /// подключение к брокеру.
     ///
     void start();
     ///
@@ -113,15 +225,29 @@ class Connector
                                         ///< используемого экземпляром
                                         ///< данного класса.
     std::shared_ptr< boost::asio::io_service::work >
-      m_sentinel; ///< "Сторож", не дает циклу ввода/вывода boost::asio
-                  ///< завершиться до завершения работы экземпляра данного класса.
-    std::shared_ptr< AMQP::Connection > m_amqpConnection;
-    std::shared_ptr< ConnectionHandler > m_connectionHandler;
+      m_sentinel; ///< "Сторож", не дает циклу службы ввода/вывода, заданной в
+                  ///< конструторе завершиться до окончания работы в
+                  ///< экземпляре данного класса. Работа происходит при
+                  ///< подключении (синхронном иили нет) к брокеру и после
+                  ///< вызова run(). Завершается в случае удачного/неудачного
+                  ///< подключения и перед запуском функции обратного вызова
+                  ///< при отключении от брокера. При удачном подключении
+                  ///< функция обратного вызова запускается после завершения
+                  ///< работы.
+    std::shared_ptr< AMQP::Connection > m_amqpConnection; ///< Указатель на
+                                                          ///< класс соединения
+                                                          ///< с брокером из
+                                                          ///< библиотеки
+                                                          ///< AMQP-CPP.
+    std::shared_ptr< ConnectionHandler > m_connectionHandler; ///< Указатель
+                                                              ///< на обработчик
+                                                              ///< соединения с
+                                                              ///< брокером.
     bool m_exiting, ///< Признак запуска штатной остановки через stop().
          m_connectionHandlerReady; ///< Признак готовности обработчика
                                    ///< соединения с брокером к работе.
     StartedCallback m_startedCb; ///< Обратный вызов после установления
-                                 ///< соединения с брокером.
+                                 ///< успешного соединения с брокером.
     ExitCallback m_exitCb; ///< Обратный вызов при завершении работы.
 };
 
